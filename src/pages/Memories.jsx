96a44ff5f4cbcf6ef7c6d3007748c85c36ff402e
abubakar shiddiq <abenk223jr@gmail.com>
2026-03-08 13:16:08 +0800

    bari

--- src/pages/Memories.jsx
+++ src/pages/Memories.jsx
@@ -42,10 +42,10 @@ const Memories = ({ isIOS }) => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.6 }}
             >
-              Maaf kalau selama ini aku pernah cuek 😞, terlalu sibuk 😔, atau
-              kurang peka sama perasaanmu . Maaf kalau kadang aku bikin kamu
-              kecewa, salah paham, atau merasa sendiri 😓. Aku gak pernah
-              berniat nyakitin kamu 🥺. Semua itu karena aku manusia biasa yang
+              Maaf kalau selama ini aku pernah cuek, terlalu sibuk, atau
+              kurang peka sama perasaanmu. Maaf kalau kadang aku bikin kamu
+              kecewa, salah paham, atau merasa sendiri. Aku gak pernah
+              berniat nyakitin kamu. Semua itu karena aku manusia biasa yang
               masih belajar mencintaimu dengan lebih baik .
             </motion.p>
             <motion.p
@@ -53,15 +53,15 @@ const Memories = ({ isIOS }) => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 1.2 }}
             >
-              Terima kasih sudah sabar, sudah tetap di sini 🫶. Aku akan terus
-              belajar jadi lebih baik lagi... untuk kamu 💗
+              Terima kasih sudah sabar, sudah tetap di sini. Aku akan terus
+              belajar jadi lebih baik lagi... untuk kamu
             </motion.p>
             <motion.p
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 1.8 }}
             >
-              🙏🏻 Maaf ya, dan makasih karena kamu tetap percaya sama aku.
+              Maaf ya, dan makasih karena kamu tetap percaya sama aku.
             </motion.p>
           </div>
         </motion.div>
